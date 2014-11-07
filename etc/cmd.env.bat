63b216258b22917f8e977c8401a7fd20285354da
Ildar Shaimordanov <ildar.shaimordanov@gmail.com>
2014-11-07 10:52:09 +0400

    cmd.env.bat: env improved

--- etc/cmd.env.bat
+++ etc/cmd.env.bat
@@ -44,38 +44,6 @@ if exist "%SEVENZIP_HOME%" set "PATH=%PATH%;%SEVENZIP_HOME%"
 
 :: ========================================================================
 ::
-:: SVN
-::
-:: ========================================================================
-
-set "SVN_HOME=%TEA_HOME%\opt\svn-1.8.5"
-if exist "%SVN_HOME%" set "PATH=%PATH%;%SVN_HOME%\bin"
-
-:: ========================================================================
-::
-:: Git
-:: (prepend in PATH for using unix tools "find" and "sort")
-::
-:: ========================================================================
-
-:: set "GIT_HOME=C:\Program Files (x86)\Git"
-set "GIT_HOME=%TEA_HOME%\vendors\msysgit"
-:: if exist "%GIT_HOME%" set "PATH=%PATH%;%GIT_HOME%\cmd"
-if exist "%GIT_HOME%" set "PATH=%GIT_HOME%\bin;%GIT_HOME%\mingw\bin;%GIT_HOME%\cmd;%GIT_HOME%\share\vim\vim74;%PATH%"
-
-:: ========================================================================
-::
-:: Perl
-:: (prepend in PATH for covering old Perl version in msysgit)
-::
-:: ========================================================================
-
-:: set "PERL_HOME=%TEA_HOME%\vendors\strawberryPerl-5.8.8.3"
-set "PERL_HOME=%TEA_HOME%\vendors\StrawberryPerl-5.16.2"
-if exist "%PERL_HOME%" set "PATH=%PERL_HOME%\perl\site\bin;%PERL_HOME%\perl\bin;%PERL_HOME%\c\bin;%PATH%"
-
-:: ========================================================================
-::
 :: PuTTY, curl etc
 ::
 :: ========================================================================
@@ -140,16 +108,7 @@ set "PATH=%PATH%;%TEA_HOME%\opt\vorbis-tools"
 set "PATH=%PATH%;%TEA_HOME%\opt\flac-x32"
 :: set "PATH=%PATH%;%TEA_HOME%\opt\flac-x64"
 
-if exist "%TEA_HOME%\opt\lame" set "PATH=%PATH%;%TEA_HOME%\opt\lame"
-
-:: ========================================================================
-::
-:: Unix tools (Cygwin, GoW, GnuWin32, UnixUtils, Win-Bash etc)
-::
-:: ========================================================================
-
-if exist "%TEA_HOME%\bin\x.bat" call "%TEA_HOME%\bin\x.bat" /set gow-git
-if defined UNIX_HOME set "PATH=%PATH%;%UNIX_HOME%\bin"
+set "PATH=%PATH%;%TEA_HOME%\opt\lame"
 
 :: ========================================================================
 ::
@@ -163,6 +122,7 @@ for %%d in (
 	"C:\Program Files\Java\jre"
 	"C:\Program Files (x86)\Java\jre"
 ) do if not defined JAVA_HOME for /d %%p in ( "%%~d*" ) do set "JAVA_HOME=%%p"
+
 if defined JAVA_HOME set "PATH=%PATH%;%JAVA_HOME%\bin"
 
 :: ========================================================================
@@ -172,6 +132,70 @@ if defined JAVA_HOME set "PATH=%PATH%;%JAVA_HOME%\bin"
 :: ========================================================================
 
 if exist "C:\Program Files\Oracle\VirtualBox" set "PATH=%PATH%;C:\Program Files\Oracle\VirtualBox"
+
+:: ========================================================================
+::
+:: SVN
+::
+:: ========================================================================
+
+set "SVN_HOME=%TEA_HOME%\opt\svn-1.8.5"
+if exist "%SVN_HOME%" set "PATH=%PATH%;%SVN_HOME%\bin"
+
+:: ========================================================================
+::
+:: Git
+::
+:: ========================================================================
+
+:: set "GIT_HOME=C:\Program Files (x86)\Git"
+set "GIT_HOME=%TEA_HOME%\vendors\msysgit"
+
+:: if exist "%GIT_HOME%" set "PATH=%PATH%;%GIT_HOME%\cmd"
+if exist "%GIT_HOME%" set "PATH=%GIT_HOME%\bin;%GIT_HOME%\mingw\bin;%GIT_HOME%\cmd;%GIT_HOME%\share\vim\vim74;%PATH%"
+
+:: ========================================================================
+::
+:: Unix tools (Cygwin, GoW, GnuWin32, MSysGit, UnixUtils, Win-Bash etc)
+::
+:: ========================================================================
+
+:: Set the location for unix tools as you want
+:: set "UNIX_HOME=%TEA_HOME%\vendors\x-cygwin"
+:: set "UNIX_HOME=%TEA_HOME%\vendors\x-gnuwin32"
+set "UNIX_HOME=%TEA_HOME%\vendors\x-gow-git"
+:: set "UNIX_HOME=%TEA_HOME%\vendors\x-msysgit"
+:: set "UNIX_HOME=%TEA_HOME%\vendors\x-unxutils"
+:: set "UNIX_HOME=%TEA_HOME%\vendors\x-win-bash"
+
+if defined UNIX_HOME set "PATH=%UNIX_HOME%\bin;%PATH%"
+
+:: What is the HOME directory?
+:: http://gnuwin32.sourceforge.net/faq.html
+set "HOME=%TEA_HOME%"
+
+:: The program aborts with the message: cannot create file /tmp/...
+:: http://gnuwin32.sourceforge.net/faq.html
+set "TMPDIR=%TEMP%"
+set "TMP=%TEMP%"
+
+:: How can I disable native language support?
+:: http://gnuwin32.sourceforge.net/faq.html
+set "LANG=en"
+set "LANGUAGE=en"
+
+:: ========================================================================
+::
+:: Perl
+:: (prepend in PATH to overrride other Perls from other places)
+::
+:: ========================================================================
+
+:: set "PERL_HOME=%TEA_HOME%\vendors\strawberryPerl-5.8.8.3"
+set "PERL_HOME=%TEA_HOME%\vendors\StrawberryPerl-5.16.2"
+
+:: if defined PERL_HOME set "PATH=%PATH%;%PERL_HOME%\perl\site\bin;%PERL_HOME%\perl\bin;%PERL_HOME%\c\bin"
+if defined PERL_HOME set "PATH=%PERL_HOME%\perl\site\bin;%PERL_HOME%\perl\bin;%PERL_HOME%\c\bin;%PATH%"
 
 :: ========================================================================
 ::
