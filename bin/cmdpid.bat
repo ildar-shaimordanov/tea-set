cf2a528cd29a4448b5abb334f49d319c9c67935a
Ildar Shaimordanov <ildar.shaimordanov@gmail.com>
2014-12-20 06:51:12 +0400

    cmdpid.bat added

---
+++ bin/cmdpid.bat
@@ -0,0 +1,29 @@
+@echo off
+
+if "%~1" == "/?" goto :help
+if "%~1" == "-?" goto :help
+
+if /i "%~1" == "/h" goto :help
+if /i "%~1" == "-h" goto :help
+
+
+for %%p in ( powershell.exe ) do if "%%~$PATH:p" == "" (
+	>&2 echo:%%p required.
+	exit /b 1
+)
+
+
+:cmdpid
+for /f "tokens=*" %%p in ( '
+	set "PPID=(Get-WmiObject Win32_Process -Filter ProcessId=$P).ParentProcessId" ^& ^
+	call powershell -NoLogo -NoProfile -Command "$P = $pid; $P = %%PPID%%; %%PPID%%"
+' ) do set CMDPID=%%p
+
+goto :EOF
+
+
+:help
+echo:Calculates the Process ID of the Command Prompt 
+echo:and assigns it to the CMDPID variable.
+echo:
+echo:Usage: %~n0
