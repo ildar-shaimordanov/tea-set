ef18d1b73b5a661d105dd52c3ed9d31f1c339dd3
Ildar Shaimordanov <ildar.shaimordanov@gmail.com>
2014-12-24 00:42:07 +0400

    cmdpid.bat: more flexible script

--- bin/cmdpid.bat
+++ bin/cmdpid.bat
@@ -7,18 +7,51 @@ if /i "%~1" == "/h" goto :help
 if /i "%~1" == "-h" goto :help
 
 
-for %%p in ( powershell.exe ) do if "%%~$PATH:p" == "" (
-	>&2 echo:%%p required.
-	exit /b 1
+for %%p in ( powershell.exe wmic.exe ) do (
+	if not "%%~$PATH:p" == "" goto :cmdpid.by.%%~np
 )
 
+>&2 echo:Unable to retrieve Process ID
+exit /b 1
 
-:cmdpid
+
+:: http://www.dostips.com/forum/viewtopic.php?p=38806#p38806
+:cmdpid.by.powershell
 for /f "tokens=*" %%p in ( '
 	set "PPID=(Get-WmiObject Win32_Process -Filter ProcessId=$P).ParentProcessId" ^& ^
 	call powershell -NoLogo -NoProfile -Command "$P = $pid; $P = %%PPID%%; %%PPID%%"
 ' ) do set CMDPID=%%p
 
+goto :EOF
+
+
+:: http://www.dostips.com/forum/viewtopic.php?p=38870#p38870
+:cmdpid.by.wmic
+setlocal disabledelayedexpansion
+
+:cmdpid.by.wmic.getlock
+set "cmdpid.lock=%TEMP%\%~nx0.%time::=.%.lock"
+set "cmdpid.uid=%cmdpid.lock:\=:b%"
+set "cmdpid.uid=%cmdpid.uid:,=:c%"
+set "cmdpid.uid=%cmdpid.uid:'=:q%"
+set "cmdpid.uid=%cmdpid.uid:_=:u%"
+
+setlocal enableDelayedExpansion
+set "cmdpid.uid=!cmdpid.uid:%%=:p!"
+endlocal & set "cmdpid.uid=%cmdpid.uid%"
+
+2>nul ( 9>"%cmdpid.lock%" (
+
+for /f "skip=1 delims=" %%A in ( '
+	wmic process where "Name='cmd.exe' and CommandLine like '%%<%cmdpid.uid%>%%'" get ParentProcessID
+' ) do for %%B in ( %%A ) do set "CMDPID=%%B"
+(call )
+
+)) || goto :cmdpid.by.wmic.getlock
+
+del "%cmdpid.lock%" 2>nul
+
+endlocal & set "CMDPID=%CMDPID%"
 goto :EOF
 
 
