58193283bee446eebf008534ecce09aaa8c44cac
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2015-09-11 19:21:32 +0400

    improve Far3.bat

--- Far3.bat
+++ Far3.bat
@@ -26,7 +26,7 @@ set "FAR_CONF=%~dp0etc\Far3"
 
 :: FAR startup options
 :: It can be redeclared in "cmd.env.bat"
-set "FAR_OPTS=/w"
+set "FAR_OPTS=/w /p"%FAR_HOME%\Plugins;%FAR_CONF%\Profile\Plugins;%ConEmuDir%\Plugins\ConEmu""
 
 if exist "%~dp0cmd.env.bat" call "%~dp0cmd.env.bat"
 
@@ -43,12 +43,10 @@ if not exist "%FAR_HOME%\Far.exe.ini" (
 		call :far.exe.ini_create >"%FAR_HOME%\Far.exe.ini"
 	)
 	if errorlevel 1 (
-		call :error "Cannot copy %FAR_CONF%\Far.exe.ini to %FAR_HOME%"
+		call :error "Cannot create %FAR_CONF%\Far.exe.ini"
 		goto :EOF
 	)
 )
-
-call :conemu_plugin_check
 
 echo:Starting %FAR_NAME%
 
@@ -93,102 +91,6 @@ echo:DefaultLanguage=Russian
 echo:
 echo:ReadOnlyConfig=0
 goto :EOF
-
-:: ========================================================================
-
-:conemu_plugin_check
-if not defined ConEmuDir goto :EOF
-if not defined ConEmuBuild goto :EOF
-
-setlocal
-
-set "far_PluginDir=%FAR_CONF%\Profile\Plugins"
-set "far_ConEmuDir=%far_PluginDir%\ConEmu"
-set "far_ConEmuFile=%far_ConEmuDir%\ConEmu.dll"
-set "far_ConEmuBuild="
-
-for %%f in ( powershell.exe robocopy.exe ) do if "%%~$PATH:f" == "" (
-	echo:
-	echo:*** ERROR ***
-	echo:%%f not found in PATH. Unable to continue checking.
-	echo:
-	echo:Copy the folder manually by yourself
-	echo:From : "%ConEmuDir%\plugins\ConEmu"
-	echo:To   : "%far_ConEmuDir%"
-	echo:
-	goto :EOF
-)
-
-set "far_cmd_del=rd /s /q "%far_ConEmuDir%""
-set "far_cmd_copy=robocopy "%ConEmuDir%\plugins\ConEmu" "%far_ConEmuDir%" /S /E >nul"
-
-if not exist "%far_ConEmuFile%" (
-	echo:
-	echo:*** WARNING ***
-	echo:No ConEmu plugin found
-	echo:
-	call :conemu_plugin_copy
-	goto :EOF
-)
-
-:: How to read ProductVersion by PowerShell
-:: http://stackoverflow.com/a/13118517/3627676
-for /f "tokens=*" %%b in ( '
-	powershell -NoLogo -NoProfile -Command "(Get-Item '%far_ConEmuFile%').VersionInfo.ProductVersion"
-' ) do (
-	set "far_ConEmuBuild=%%b"
-)
-
-if "%ConEmuBuild%" == "%far_ConEmuBuild%" goto :EOF
-
-echo:
-echo:*** WARNING ***
-echo:The existing plugin does not match the actual version of ConEmu
-echo:
-echo:ConEmu found    : %far_ConEmuBuild%
-echo:ConEmu required : %ConEmuBuild%
-echo:
-echo:Using of another version of ConEmu can cause errors
-echo:It is extremely recommended to update the plugin for Far Manager
-echo:
-
-call :conemu_plugin_lockstate && call :conemu_plugin_del && call :conemu_plugin_copy && goto :EOF
-
-echo:
-echo:Perhaps, the plugin is locked by another application (Far Manager or 
-echo:something else). Stop it or unlock the plugin and restart this file 
-echo:again. Also you can perform the following commands by yourself:
-echo:
-echo:%far_cmd_del%
-echo:%far_cmd_copy%
-echo:
-
-goto :EOF
-
-:: ========================================================================
-
-:conemu_plugin_lockstate
-:: How to check if file is locked
-:: http://stackoverflow.com/a/24994480/3627676
-powershell -NoLogo -NoProfile -Command "try { [IO.File]::OpenWrite('%far_ConEmuFile%').Close(); } catch { exit 1; }"
-goto :EOF
-
-:: ========================================================================
-
-:conemu_plugin_del
-echo:*** Removing incompatible version
-%far_cmd_del%
-goto :EOF
-
-:: ========================================================================
-
-:conemu_plugin_copy
-:: For Robocopy exit codes see the link:
-:: http://ss64.com/nt/robocopy-exit.html
-echo:*** Copying the actual version
-%far_cmd_copy%
-if errorlevel 8 goto :EOF
-exit /b 0
 
 :: ========================================================================
 
