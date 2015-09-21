c9c96de13a8ac269182f8f1e8086879eee3f6be6
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2015-09-21 04:32:56 +0400

    update clink.bat startup

--- clink.bat
+++ clink.bat
@@ -5,6 +5,4 @@ if exist "%~dp0vendors\clink\clink_x%PROCESSOR_ARCHITECTURE:~-2%.exe" "%~dp0vend
 if exist "%~dp0cmd.env.bat" call "%~dp0cmd.env.bat"
 
 :: Integrate Git into prompt
-if defined ConEmuBaseDir if exist "%ConEmuBaseDir%\IsConEmu.cmd" (
-	call "%ConEmuBaseDir%\IsConEmu.cmd" >nul && prompt $p$s{git}$_$g$s
-)
+call "%ConEmuBaseDir%\IsConEmu.cmd" >nul && prompt $p$s{git}$_$g$s
