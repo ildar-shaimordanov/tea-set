385dcfad8dd922bcf43607eb968a126c0f223f6a
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2015-09-22 17:33:45 +0400

    improve ftpd.bat

--- bin/ftpd.bat
+++ bin/ftpd.bat
@@ -7,14 +7,18 @@ set "FTP_NAME=FileZilla Server.exe"
 set "FTP_CONF=FileZilla Server.xml"
 set "FTP_CTRL=FileZilla Server Interface.exe"
 
-if /i "%~1" == "start" for /f "tokens=2 delims==>" %%d in ( '
-	findstr /i /r /c:"<Permission Dir=\".*\">" "%FTP_HOME%\%FTP_CONF%"
-' ) do (
-	if exist %%~dd if not exist %%d md %%d
-)
+if /i "%~1" == "start" call :prepare-dirs
+if /i "%~1" == "compat-start" call :prepare-dirs
 
 for %%a in ( install uninstall start stop ) do if /i "%~1" == "%%~a" (
 	"%FTP_HOME%\%FTP_NAME%" /%%~a
+	endlocal
+	goto :EOF
+
+)
+
+for %%a in ( start stop ) do if /i "%~1" == "compat-%%~a" (
+	start "" "%FTP_HOME%\%FTP_NAME%" /compat /%%~a
 	endlocal
 	goto :EOF
 
@@ -41,16 +45,28 @@ if /i "%~1" == "control" (
 :: https://wiki.filezilla-project.org/Command-line_arguments_%28Server%29
 echo:Usage:
 echo:	%~n0 INSTALL ^| UNINSTALL
-echo:	%~n0 START ^| STOP
+echo:	%~n0 START ^| STOP ^| COMPAT-START ^| COMPAT-STOP
 echo:	%~n0 STATUS ^| CONTROL
 echo:
 echo:INSTALL		install or uninstall the service
 echo:UNINSTALL
 echo:START		start or stop the server as the service
 echo:STOP
+echo:COMPAT-START	start or stop the server as the application
+echo:COMPAT-STOP
 echo:STATUS		check the status of processes and services
 echo:CONTROL		launch the controlling client
 
 endlocal
+goto :EOF
+
+
+:prepare-dirs
+for /f "tokens=2 delims==>" %%d in ( '
+	findstr /i /r /c:"<Permission Dir=\".*\">" "%FTP_HOME%\%FTP_CONF%"
+' ) do (
+	if exist %%~dd if not exist %%d md %%d
+)
+goto :EOF
 
 :: EOF
