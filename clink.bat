885186428d4f1359cf4807712ef7df76ac792e15
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2015-09-24 01:03:47 +0400

    no dependency on ConEmu in clink runner

--- clink.bat
+++ clink.bat
@@ -5,4 +5,4 @@ if exist "%~dp0vendors\clink\clink_x%PROCESSOR_ARCHITECTURE:~-2%.exe" "%~dp0vend
 if exist "%~dp0cmd.env.bat" call "%~dp0cmd.env.bat"
 
 :: Integrate Git into prompt
-call "%ConEmuBaseDir%\IsConEmu.cmd" >nul && prompt $p$s{git}$_$g$s
+prompt $p$s{git}$_$g$s
