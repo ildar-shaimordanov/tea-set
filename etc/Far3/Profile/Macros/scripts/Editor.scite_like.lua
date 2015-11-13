edfb14b3c876d4ac3ab5c29ec9698016d2f529fb
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2015-11-13 16:17:28 +0400

    update SciTE-like lua script for FAR3

--- etc/Far3/Profile/Macros/scripts/Editor.scite_like.lua
+++ etc/Far3/Profile/Macros/scripts/Editor.scite_like.lua
@@ -8,6 +8,8 @@
 -- * Type or delete (Del, BS) the text.
 
 local F=far.Flags
+local Map = {Space=" ", Tab="\t", BackSlash="\\"}
+
 Event {
   group="EditorInput";
   action=function(Rec)
@@ -16,34 +18,46 @@ Event {
       if EI and EI.BlockType==F.BTYPE_COLUMN then
         local cur = editor.GetString()
         if cur and cur.SelStart>0 and cur.SelEnd<=cur.SelStart and EI.CurPos>=cur.SelStart then
-          local char = Rec.UnicodeChar:match("[ \t%w%p%^$+=~`<>|]")
-          local key = not char and far.InputRecordToName(Rec)
-          if char or key=="Del" or key=="BS" then
+          local key = far.InputRecordToName(Rec)
+          local char = key and (Map[key] or key:match("^.$"))
+          if key=="Del" or key=="BS" or key=="Left" or key=="Right" or char then
             local lnum = EI.BlockStartLine
-            editor.UndoRedo(nil,F.EUR_BEGIN)
+            local newpos, clean = nil, true
             while true do
               local line = editor.GetString(nil,lnum)
               if not line or line.SelStart <= 0 then break end
               local pos = editor.TabToReal(nil,lnum,EI.CurTabPos)
               local s = line.StringText
-              if char then
+              if key == "Del" then
+                if pos <= line.StringLength then
+                  if clean then editor.UndoRedo(nil,F.EUR_BEGIN); clean=false end
+                  editor.SetString(nil, lnum, s:sub(1,pos-1)..s:sub(pos+1))
+                end
+              elseif key == "Right" then
+                newpos = EI.CurPos + 1
+              elseif key == "Left" then
+                if pos == 1 then return true end
+                newpos = EI.CurPos - 1
+              elseif key == "BS" then
+                if pos == 1 then return true end
+                if pos <= line.StringLength+1 then
+                  if clean then editor.UndoRedo(nil,F.EUR_BEGIN); clean=false end
+                  editor.SetString(nil, lnum, s:sub(1,pos-2)..s:sub(pos))
+                end
+                newpos = EI.CurPos - 1
+              elseif char then
                 if pos > line.StringLength+1 then
                   s = s..(" "):rep(pos-line.StringLength-1)
                 end
+                if clean then editor.UndoRedo(nil,F.EUR_BEGIN); clean=false end
                 editor.SetString(nil, lnum, s:sub(1,pos-1)..char..s:sub(pos+EI.Overtype))
-                editor.SetPosition(nil,EI.CurLine,EI.CurPos+1)
-              elseif key=="Del" then
-                editor.SetString(nil, lnum, s:sub(1,pos-1)..s:sub(pos+1))
-              elseif key=="BS" and pos>1 then
-                editor.SetString(nil, lnum, s:sub(1,pos-2)..s:sub(pos))
-                editor.SetPosition(nil,EI.CurLine,EI.CurPos-1)
+                newpos = EI.CurPos + 1
               end
               lnum = lnum + 1
             end
-            editor.UndoRedo(nil,F.EUR_END)
-            editor.Select(nil,"BTYPE_COLUMN",EI.BlockStartLine,
-              cur.SelStart + (char and 1 or (key=="BS" and EI.CurPos>1 and -1) or 0),
-              1,lnum-EI.BlockStartLine)
+            if not clean then editor.UndoRedo(nil,F.EUR_END) end
+            if newpos then editor.SetPosition(nil, EI.CurLine, newpos) end
+            editor.Select(nil, "BTYPE_COLUMN", EI.BlockStartLine, newpos or cur.SelStart, 1, lnum-EI.BlockStartLine)
             editor.Redraw()
             return true
           end
