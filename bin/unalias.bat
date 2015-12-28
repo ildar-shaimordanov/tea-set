31faf68f6b91fb97153b37208c46d81e69d9b8d7
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2015-12-28 10:09:00 +0400

    improve unalias.bat script

--- bin/unalias.bat
+++ bin/unalias.bat
@@ -1,16 +1,27 @@
 @echo off
 
+if "%~1" == "" goto :help
+
 if "%~1" == "/?" goto :help
 if "%~1" == "-?" goto :help
 
 if /i "%~1" == "/h" goto :help
 if /i "%~1" == "-h" goto :help
 
-doskey %1=
+if /i "%~1" == "/a" goto :unalias.all
+if /i "%~1" == "-a" goto :unalias.all
+
+for %%m in ( %* ) do doskey %%m=
+goto :EOF
+
+:unalias.all
+for /f "tokens=1 delims==" %%m in ( 'doskey /macros' ) do doskey %%m=
 goto :EOF
 
 :help
-echo:Removes the associated name from the list of defined macros.
+echo:Removes the associated names from the list of defined macros.
 echo:For more details, see DOSKEY /?
 echo:
-echo:Usage: unalias name
+echo:Usage: unalias [-a] name [name ...]
+echo:
+echo:    -a Remove all aliases
