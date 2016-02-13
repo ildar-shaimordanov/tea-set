faa268e84aed6018373978144b7f0191874dec46
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2016-02-13 23:56:00 +0400

    simplify clink runner

--- clink.bat
+++ clink.bat
@@ -1,6 +1,6 @@
 @echo off
 
-if exist "%~dp0vendors\clink\clink_x%PROCESSOR_ARCHITECTURE:~-2%.exe" "%~dp0vendors\clink\clink_x%PROCESSOR_ARCHITECTURE:~-2%.exe" inject --quiet --profile "%~dp0etc\clink"
+call "%~dp0vendors\clink\clink.bat" inject --quiet --profile "%~dp0etc\clink"
 
 if exist "%~dp0shellenv.bat" call "%~dp0shellenv.bat"
 
