279969846e27cf254f236946a6057606d8657039
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2016-03-10 13:33:57 +0400

    enable customizale call of clink

--- clink.bat
+++ clink.bat
@@ -1,6 +1,12 @@
 @echo off
 
-call "%~dp0vendors\clink\clink.bat" inject --quiet --profile "%~dp0etc\clink"
+set "CLINK_NAME=clink"
+
+if exist "%~dp0vendors\clink.bat" call "%~dp0vendors\clink.bat"
+
+if defined CLINK_NAME if exist "%~dp0vendors\%CLINK_NAME%\clink.bat" (
+	call "%~dp0vendors\%CLINK_NAME%\clink.bat" inject --quiet --profile "%~dp0etc\clink"
+)
 
 if exist "%~dp0shellenv.bat" call "%~dp0shellenv.bat"
 
