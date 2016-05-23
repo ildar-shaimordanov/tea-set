f465069eea473f5da37401c95adb3bf2ac7f1431
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2016-05-23 04:00:56 +0400

    improve autostart far3+lua script

--- etc/Far3/Profile/Macros/scripts/AutoStart.Shell.lua
+++ etc/Far3/Profile/Macros/scripts/AutoStart.Shell.lua
@@ -4,25 +4,10 @@ Macro {
 	key = "";
 	flags = "RunAfterFARStart";
 	action = function()
-		-- The idea with turn off/on key bar was borrowed from
-		-- http://forum.farmanager.com/viewtopic.php?p=127121#p127121
-		local toggleKeyBar = Far.GetConfig"Screen.KeyBar" and "CtrlB"
-		Keys(toggleKeyBar)
-
+		-- http://forum.farmanager.com/viewtopic.php?p=138007#p138007
 		panel.GetUserScreen()
-
-		-- save the current locale
-		local l = os.setlocale()
-
-		os.setlocale("", "time")
-		io.write(string.format("[ %s ]\n\n", os.date()))
-
-		-- restore previous locale
-		os.setlocale(l)
-
+		win.system("echo:Started Far3")
+		win.system("echo:[ %DATE% %TIME% ]")
 		panel.SetUserScreen()
-
-		Keys(toggleKeyBar)
-		-- Keys("e c h o : & e c h o : [ Space % D A T E % Space % T I M E % Space ] & e c h o : Enter");
 	end;
 }
