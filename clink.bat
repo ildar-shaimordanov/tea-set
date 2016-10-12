92b098d60b661d231028be064abbe7024acf141e
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2016-10-12 13:03:50 +0400

    simplify clink invocation

--- clink.bat
+++ clink.bat
@@ -4,8 +4,11 @@ set "CLINK_NAME=clink"
 
 if exist "%~dp0vendors\identify.bat" call "%~dp0vendors\identify.bat" app clink
 
-if not defined CLINK_NAME goto :continue
-if not exist "%~dp0vendors\%CLINK_NAME%\clink.bat" goto :continue
+if exist "%~dp0shellenv.bat" call "%~dp0shellenv.bat"
+set "HOME=%~dp0home"
+
+if not defined CLINK_NAME goto :EOF
+if not exist "%~dp0vendors\%CLINK_NAME%\clink.bat" goto :EOF
 
 :: ========================================================================
 
@@ -14,11 +17,6 @@ call "%~dp0vendors\%CLINK_NAME%\clink.bat" inject --quiet --profile "%~dp0etc\cl
 
 :: Integrate Git into prompt
 prompt $p$s{git}$_$g$s
-
-:: ========================================================================
-
-:continue
-if exist "%~dp0shellenv.bat" call "%~dp0shellenv.bat"
 
 :: ========================================================================
 
