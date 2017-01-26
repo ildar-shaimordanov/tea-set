19ce7f4116103140383e5f1c661f6d898a2c5119
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2017-01-26 12:09:58 +0400

    cosmetic changes to Far3 macros

--- etc/Far3/Profile/Macros/scripts/AutoStart.Shell.lua
+++ etc/Far3/Profile/Macros/scripts/AutoStart.Shell.lua
@@ -1,10 +1,12 @@
 Macro {
-	description = "";
+	description = "Display date/time of Far invocation";
 	area = "Shell";
 	key = "";
 	flags = "RunAfterFARStart";
 	action = function()
 		-- http://forum.farmanager.com/viewtopic.php?p=138007#p138007
+		-- Скрыть "вводимую" команду при старте FAR
+		-- Hide an "entered" command at starting of FAR
 		panel.GetUserScreen()
 		win.system("echo:Started Far3")
 		win.system("echo:[ %DATE% %TIME% ]")
