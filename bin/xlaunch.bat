7b5004ed023bc740424a058edff88eacc3fc691f
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2018-08-06 17:26:31 +0400

    simplify xlaunch.bat from the previous commit

--- bin/xlaunch.bat
+++ bin/xlaunch.bat
@@ -4,7 +4,9 @@ for /d %%d in (
 	"%~dp0..\vendors\xsrv"
 	"%~dp0..\vendors\VcXsrv*"
 	"%~dp0..\vendors\Xming*"
-) do if exist "%%~d" if exist "%~dp0..\etc\misc\xsrv\xsrv-multiwindow.xlaunch" (
+) do if exist "%%~d" for %%n in (
+	XLaunch
+) do if exist "%%~d\%%~n.exe" (
 	start "" "%%~d\XLaunch.exe" -run "%~dp0..\etc\misc\xsrv\xsrv-multiwindow.xlaunch"
 	goto :EOF
 )
