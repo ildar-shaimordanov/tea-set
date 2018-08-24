c59dbd2638930272942f4cc695b3cb4fc90e34e8
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2018-08-24 15:13:09 +0400

    fix bin\xsrv.bat

--- bin/xsrv.bat
+++ bin/xsrv.bat
@@ -5,9 +5,8 @@ for /d %%d in (
 	"%~dp0..\vendors\VcXsrv*"
 	"%~dp0..\vendors\Xming*"
 ) do if exist "%%~d" for %%n in (
-	vcxsrv
-	Xming
+	vcxsrv Xming
 ) do if exist "%%~d\%%~n.exe" (
-	start "%%~d\%%~n.exe" :0 -multiwindow -clipboard
+	start "" "%%~d\%%~n.exe" :0 -multiwindow -clipboard
 	goto :EOF
 )
