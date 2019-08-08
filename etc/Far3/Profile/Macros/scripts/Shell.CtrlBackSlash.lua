c76fa43c60261e54692507fc475128d15f28e531
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2019-08-08 21:53:44 +0400

    update Far3 script extending "Ctrl+\" action (continue commit 65749f2)

--- etc/Far3/Profile/Macros/scripts/Shell.CtrlBackSlash.lua
+++ etc/Far3/Profile/Macros/scripts/Shell.CtrlBackSlash.lua
@@ -1,13 +1,21 @@
--- https://forum.farmanager.com/viewtopic.php?p=154568#p154568
+-- https://forum.farmanager.com/viewtopic.php?p=154581#p154581
 
 Macro {
 	area = "Shell";
 	key = "CtrlBackSlash";
 	description = "Shell: Cd \\ and highlght the upper level dir of the previous location";
 	action = function()
-		local path = APanel.Path~="" and APanel.Path or APanel.Current:find("\\") and APanel.Current
-		if path then
-			local root, dir = path:match("([^\\]*\\)([^\\]*)")
+		local path = APanel.Path
+		local root, dir
+		if APanel.Plugin then -- plugin panel
+			path = APanel.Current:find("\\") and APanel.Current or path
+			root, dir = path:match("([^\\]*\\)([^\\]*)")
+		elseif APanel.Path:find("^\\\\") then -- network share
+			root, dir = path:match("^(\\\\[^\\]*\\%w%$\\)([^\\]*)")
+		else -- local directory
+			root, dir = path:match("([^\\]*\\)([^\\]*)")
+		end
+		if root then
 			Panel.SetPath(0, root, dir)
 		else
 			Keys("CtrlBackSlash") -- default Far action
