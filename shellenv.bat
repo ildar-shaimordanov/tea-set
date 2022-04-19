7eb402a94265b226d9c3713835d95d38dd754bc1
Ildar Shaimordanov <ildar.shaimordanov@gmail.com>
2022-04-19 21:20:05 +0300

    simplify shellenv.bat

--- shellenv.bat
+++ shellenv.bat
@@ -1,81 +1,21 @@
-:: ========================================================================
-::
 :: This file is a helper script to simplify setting up of the %PATH% and 
-:: other needful environment variables.
-::
-:: ========================================================================
+:: other useful environment variables.
 
 @echo off
 
-:: ========================================================================
-::
-:: Escape double execution
-::
-:: ========================================================================
-
-if defined CMD_ENV_LOADED goto :shellenv.integration
-
-set "CMD_ENV_LOADED=1"
-
-:: ========================================================================
-::
-:: Set up useful aliases
-::
-:: ========================================================================
-
-doskey alias=if "$1" == "" ( doskey /macros ) else ( doskey $* )
-doskey unalias=doskey $1=
-doskey shellenv="%~f0"
-doskey shellinfo="%~dp0shellinfo.bat" $*
-
-:: ========================================================================
-::
 :: Tea-Set
-::
-:: ========================================================================
-
 for /f "tokens=*" %%p in ( "%~dp0." ) do set "TEA_HOME=%%~fp"
 set "TEA_HOME=C:\PROGS"
-call :shellenv.select.path "%TEA_HOME%"
+call :shellenv.set.dir "%TEA_HOME%"
 
-:: ========================================================================
-::
 :: Java
-::
-:: ========================================================================
-
 if exist "%~dp0javaenv.bat" call "%~dp0javaenv.bat"
-
-:: ========================================================================
-::
-:: Autoloading the rest of tools (for example from %TEA_HOME%\opt)
-::
-:: ========================================================================
 
 call :shellenv.from.file
 
-:: ========================================================================
-::
-:: Integration with ConEmu
-::
-:: ========================================================================
+call :shellenv.alias
 
-:shellenv.integration
-
-:: reg query "HKCU\Console" /v VirtualTerminalLevel 2>nul | find "0x1" && echo:[99999;1H
-if defined ConEmuANSI if /i "%ConEmuANSI%" == "ON" echo:[99999;1H
-
-:: Make ConEmu's environment variables available for child processes
-:: if defined ConEmuBaseDir if exist "%ConEmuBaseDir%\IsConEmu.cmd" (
-:: 	call "%ConEmuBaseDir%\IsConEmu.cmd" >nul && "%ConEmuBaseDir%\ConEmuC.exe" /export
-:: )
-
-:: Colorize the command line prompt 
-:: if defined ConEmuBaseDir if exist "%ConEmuBaseDir%\ColorPrompt.cmd" (
-:: 	call "%ConEmuBaseDir%\ColorPrompt.cmd"
-:: )
-
-:: ========================================================================
+call :shellenv.conemu
 
 goto :EOF
 
@@ -95,7 +35,7 @@ for /f "usebackq eol=; tokens=1,2,3,4 delims=	" %%1 in ( "%~dpn0.cfg" ) do (
 	) else if /i "%%~1" == "DIRS" (
 		call :shellenv.set.dirs "!auto_root!\%%~2" "%%~3" "%%~4"
 	) else if /i "%%~1" == "DIR" (
-		call :shellenv.select.path "!auto_root!\%%~2" "%%~3"
+		call :shellenv.set.dir "!auto_root!\%%~2" "%%~3"
 	)
 )
 
@@ -104,33 +44,14 @@ goto :EOF
 
 :: ========================================================================
 
-:: This routine sets home name variable (looks like XXX_HOME) to the 
-:: directory's path and adds paths to %PATH%. 
-::
-:: Do nothing, if the home variable is already defined. 
-::
-:: The routine looks for binaries located in the subdirectories (listed 
-:: below) under the home directory and adds them to %PATH% in the case 
-:: binaries are found there. 
-::
-:: Subdirectories are looked for binaries and all of then added to %PATH%: 
-:: "bin", "sbin", "usr\bin", ".".
-::
-:: By default, all the paths are appended to %PATH%. In the case when 
-:: paths should to be prepended, use "/P" option.
-::
-:: %~1 - homedir
-:: %~2 - the subdirectories list
-:: %~3 - "prepend" for prepending or nothing
-
-:: home prepend
+:: homedir [prepend]
 :shellenv.set.home
 call :shellenv.set.dirs "%~1" "bin;sbin;usr\bin" "%~2"
 goto :EOF
 
 :: ========================================================================
 
-:: root\dir subdirs prepend
+:: rootdir\dir subdirs [prepend]
 :shellenv.set.dirs
 if "%~1" == "" goto :EOF
 if "%~2" == "" goto :EOF
@@ -142,31 +63,20 @@ set "auto_unchecked=%~1\!auto_unchecked:;=;%~1\!"
 
 set "auto_path="
 for %%p in ( "%auto_unchecked:;=" "%" ) do (
-	call :shellenv.check.path "%%~p" && set "auto_path=!auto_path!;%%~p"
+	call :shellenv.check.dir "%%~p" && set "auto_path=!auto_path!;%%~p"
 )
 
 if defined auto_path set "auto_path=!auto_path:~1!"
 
-call :shellenv.concat.path "%~3"
+call :shellenv.concat "%~3"
 
 endlocal & set "PATH=%PATH%"
 goto :EOF
 
 :: ========================================================================
 
-:: The routine looks for binaries located in the subdirectories (listed 
-:: below) and adds the first found subdirectory to %PATH%. 
-::
-:: Subdirectories are looked for binaries and all of then added to %PATH%: 
-:: "bin", "usr\bin", "cmd", ".".
-::
-:: By default, all the paths are appended to %PATH%. In the case when 
-:: paths should to be prepended, use "/P" option.
-::
-:: %~1 - the directory's path
-:: %~2 - "prepend" for prepending or nothing
-
-:shellenv.select.path
+:: rootdir\dir [prepend]
+:shellenv.set.dir
 if "%~1" == "" goto :EOF
 
 for %%p in (
@@ -175,9 +85,9 @@ for %%p in (
 	"%~1\cmd"
 	"%~1"
 ) do (
-	call :shellenv.check.path "%%~fp" && (
+	call :shellenv.check.dir "%%~fp" && (
 		set "auto_path=%%~fp"
-		call :shellenv.concat.path "%~2"
+		call :shellenv.concat "%~2"
 		goto :EOF
 	)
 )
@@ -186,7 +96,8 @@ goto :EOF
 
 :: ========================================================================
 
-:shellenv.check.path
+:: dir
+:shellenv.check.dir
 :: Skip if path not exists
 if not exist "%~1" exit /b 1
 
@@ -199,7 +110,8 @@ goto :EOF
 
 :: ========================================================================
 
-:shellenv.concat.path
+:: [prepend]
+:shellenv.concat
 if not defined auto_path goto :EOF
 
 if /i "%~1" == "prepend" (
@@ -207,6 +119,33 @@ if /i "%~1" == "prepend" (
 ) else (
 	set "PATH=%PATH%;%auto_path%"
 )
+goto :EOF
+
+:: ========================================================================
+
+:shellenv.alias
+doskey alias=if "$1" == "" ( doskey /macros ) else ( doskey $* )
+doskey unalias=doskey $1=
+doskey shellenv="%~f0"
+doskey shellinfo="%~dp0shellinfo.bat" $*
+goto :EOF
+
+:: ========================================================================
+
+:shellenv.conemu
+
+:: reg query "HKCU\Console" /v VirtualTerminalLevel 2>nul | find "0x1" && echo:[99999;1H
+if defined ConEmuANSI if /i "%ConEmuANSI%" == "ON" echo:[99999;1H
+
+:: Make ConEmu's environment variables available for child processes
+:: if defined ConEmuBaseDir if exist "%ConEmuBaseDir%\IsConEmu.cmd" (
+:: 	call "%ConEmuBaseDir%\IsConEmu.cmd" >nul && "%ConEmuBaseDir%\ConEmuC.exe" /export
+:: )
+
+:: Colorize the command line prompt
+:: if defined ConEmuBaseDir if exist "%ConEmuBaseDir%\ColorPrompt.cmd" (
+:: 	call "%ConEmuBaseDir%\ColorPrompt.cmd"
+:: )
 goto :EOF
 
 :: ========================================================================
