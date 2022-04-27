6a5be56ee62422dc66b8791bba22ce7523aabd04
Ildar Shaimordanov <ildar.shaimordanov@gmail.com>
2022-04-27 14:22:24 +0300

    set light green prompt for clink

--- etc/clink/clink_start.cmd
+++ etc/clink/clink_start.cmd
@@ -1,8 +1,8 @@
 @echo off
 
 :: Customize the prompt
-:: - the current disk and path in green
+:: - the current disk and path in light green
 :: - new line
 :: - the ">" character
 :: - the " " character
-prompt $E[32m$P$E[m$_$G$S
+prompt $E[92m$P$E[m$_$G$S
